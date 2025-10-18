711db08f6f937d81ec1ebfa9d479710895f4d2c5
Vinícius Santos <136592881+vinisantosx@users.noreply.github.com>
2025-10-18 12:07:59 -0300

    Adicionando Tela de Carregamento

---
+++ KinCoin/App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import TelaDeCarregamento from "./src/pages/TelaDeCarregamento";
+
+export default function App() {
+  return (
+    <TelaDeCarregamento></TelaDeCarregamento>
+  );
+}
+
+const styles = StyleSheet.create({
+});
